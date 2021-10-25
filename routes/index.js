b2221784c2ab1350f7dd4547f83430caffe53636
God of War <jamel.eggleston@gmail.com>
2021-10-25 13:03:27 -0400

    added get route and some comments

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 
+//Defining the path for api routes
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
   res.send("<h1>Wrong Route!</h1>")
 });
 
+//exporting the roter for server.js file. 
 module.exports = router;
